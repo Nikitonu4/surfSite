add27ab464ecb0f3e66b24b3764df809e659f830
Nikitonu4 <nikitonu4@gmail.com>
2021-01-24 22:58:14 +0300

    1 секция готова кроме карты

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,19 @@
 $(function () {
   $('.header__slider').slick({
     infinite: true,
-    fade: true
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="">',
+    nextArrow: ' <img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="">',
+    asNavFor: '.slider__dots',
+    autoplay: true, 
   });
+  $('.slider__dots').slick(
+    {
+ slidesToShow: 4,
+ slidesToScroll: 4,
+ asNavFor: 'header__slider',
+ autoplay: true, 
+    }
+  );
+
 });
